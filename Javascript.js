278341eee6274be8d2ae83711bfa9b03676f23d9
krismate <mitchell.kristin1@icloud.com>
2017-04-18 01:23:15 -0700

    Add files via upload

---
+++ Javascript.js
@@ -0,0 +1,7 @@
+<ul style="list-style-type:square">
+<input type="checkbox" id="grapescheckbox1" name="grapes">
+<label for="grapescheckbox1">grapes</label><br>
+<input type="checkbox" id="orangescheckbox1" name="oranges">
+<lable for="orangescheckbox1">oranges</label><br>
+<input type="checkbox" id="plumscheckbox1" name="plums">
+<label for="plumscheckbox1">plums</label><br>
